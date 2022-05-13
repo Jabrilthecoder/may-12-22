7c31c692e45374aa32f72b9f7a38a98282ae7843
Jabril Abdilahi <94321684+Jabrilthecoder@users.noreply.github.com>
2022-05-12 21:52:05 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,920 @@
+let age = 12;
+age = 12;
+
+const birthyear = 2009;
+
+const job = coder;
+//day 2 december 14 2021
+console.log(job);
+console.log(birthyear);
+console.log(age)
+const ageMustafa = now - 2023;
+console.log(age, ageMustafa);
+const firstName = 'Jabril';
+const lastName ='Abdilai';
+console.log(firstName + ' ' + lastName);
+
+let x = 10 + 5;
+x += 10;
+x *= 4;
+x++;
+x--;
+x--;
+console.log(x);
+// video two day2 december 14 2021
+console.log (age > ageMustafa);
+console.log(ageMustafa >=8);
+
+const isFullAge = age
+
+const now = 2021;
+
+const agejabril = now - 2009;
+const ageMustafa = now - 2013;
+
+console.log(now - 2009 > now - 2013);
+
+//day2 video 3 december 14 2021
+
+let x, y;
+x= y = 25-10-5;
+console.log(x,y);
+const averageAge = (agejabril + ageMustafa) /2
+console.log(agejabril, ageMustafa, averageAge);
+
+const massJabril = 105;
+const heightJabril = 62.5;
+const massMustafa = 72;
+const heightMustafa = 55.5;
+
+
+
+let firstName ="jabril";
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let thefamily="jabril,jazmine,mustafa,makial,noor, hamdi";
+
+console.log(thefamily);
+
+let javascriptisfun = true;
+console.log(javascriptisfun);
+
+console.log(typeof true);
+console.log(typeof javascriptisfun);
+console.log(typeof 12);
+console.log(typeof 'jabril');
+let age = 12
+age = 12
+console.log(age)
+// day 3 work december 15 2021 and day 4 december 18 2021
+const noorHeight = 73;
+const noorWeight = "unkown";
+
+const HamdiHeight = 71;
+const HamdiWeight = "N/A";
+
+const jazmineHeight = 63.5;
+const jazmineWeight = 108.5;
+
+console.log(noorWeight < HamdiWeight);
+console.log(noorHeight > HamdiHeight);
+
+console.log(jazmineHeight > heightJabril);
+console.log(jazmineWeight > massJabril);
+
+const makialHeight = 20;
+const makialWeight = 18;
+
+console.log (massMustafa > makialWeight);
+console.log (heightMustafa > makialHeight);
+
+//day 4 work december 19 2021
+
+const birthYEAR = 2009;
+
+let century; if (birthYear <=2000) {
+century = 20;
+} else { century =21;
+} console.log (century);
+
+
+// day 4 work december 19 2021
+
+'use strict';
+const BMIjazmine = jazmineWeight / jazmineHeight ** 2;
+const BMIjabril = massJabril / heightJabril ** 2;
+console.log(BMIjazmine, BMIjabril);
+
+IF (BMIjabril > BMIjazmine)
+ { console.log("Jabril's bmi is higher than jazmines") }
+
+  console.log("jazmines BMI is higher than jazmine")
+
+  // day 5 december 20 2021
+
+
+  const inputYear = '2009';
+  console.log(number (inputYear), inputYear);
+  console.log(number(inputYear)+ 16);
+
+  console(n)
+
+
+
+
+  const jazmine = "your feet are so long it looks like dinosaurs foot ";
+
+  console.log(jazmine);
+
+  console.log(number("javrul"));
+    console.log(string(12),12);
+
+    console.log('i am' + 12 + ' years old ');
+    console.log('12' + '4' - 4);
+
+    console.log(boolean(10));
+    console.log(boolean(null));
+    console.log(boolean(8));
+    console.log(boolean(12));
+
+const money = 100000;
+    if (money) {console.log("you need a job fatass;)");} else {console.log('are you ok you just got your rich ass scammed');}
+
+    let height = 5.2;
+    if (height) {console.log('damn!your short got to sleep'); }else {console.log('how tall are you');}
+
+    const favorite  = neegae;
+
+    if (favorite === 12) { console.log('cool! your 12 me too');
+  } else if (favorite=8) {console.log('ew 8 are you 8 and an addict to fortnite dumb smh i am an addic of mm2'); } else {console.log('favortie=im very cool');}
+
+//day 6 decemeber 22 2021
+
+const hasDriverLiscence = false;
+const imBored = true;
+
+console.log(hasDriverLiscence && imBored);
+console.log(hasDriverL || hasGood);
+console.log(!hasDriverLiscence);
+
+const isTired = True;
+
+
+
+if (hasDriversLiscense && imBored && imTired); {
+
+console.log('jabril is not able to drive')
+}
+
+//booleans today video 23 stoping point n
+
+const scoreDolphins =( 97 + 112 + 80 ) / 3;
+const scoreKoalas = (109 +95 + 50 ) /3;
+console.log(scoreDolphins, scoreKoalas);
+
+if (scoreDolphins > scoreKoalas && scoreDolphins >= 100) {console.log('dolphins win the trophy');} else if (scoreKoalas > scoreDolphins && scoreKoalas >= 100) {console.log('koalas win the trophy');} else if (scoreKoalas === scoreDolphins && scoreKoalas >= 100) { console.log('koalas win the trophy');}else if (scoreDolphins === scoreKoalas && scoreDolphins >= 100 && scoreKoalas >= 100) {console.log('both win tie!')} else {console.log('No one wins the trophy');
+}
+//day 7 switch statements dec 25 2021
+
+const day = 'monday';
+
+switch(day) {
+  case 'monday' : // day ==='monday'
+  console.log('plane yt video');
+  console.log ('yt video');
+  console.log('do my work');
+  console.log('school');
+
+    case 'friday' :
+
+    console.log('coding,kumon,reading');
+    console.log('play on phone');
+    console.log('clean kitchen for 2 hours');
+    console.log('play');
+    console.log ('go to bed');
+
+
+}
+// video 27
+"3 + 4"
+"1991"
+"true && false && !false <- statement"
+"ex of code  if (23 > 10) {const str = '23 is bigger '; } strings are expresions "
+"console.log(im $(2037 - 1991) years old ${me'}"
+
+// video 28
+
+const drink = age >= 18 ? 'wine' : 'water';
+console.log(drink);
+let drink2;
+if (age >=21) {drink2 = 'wine';
+} else {drink2 ='water';} console.log(drink2);
+console.log(`i like to drink ${age >= 18 ? 'wine' : 'water'}`);
+
+// day 7 video 29 - 33
+const credits = +300000;
+// 😁👍 nice
+const executionTax = bill <= 300000 && bill >= 30 ? bill * 0.15 :
+bill * 0.2;
+crossOriginIsolated.log(`The bill was ${bill}, the tip was ${tip}, and the total value ${bill + tip}`);
+// 😵 good luck
+
+// tip ammount  ^
+            // |  |
+
+// video 33
+
+function logger() {
+  console.log('My name is jonas')
+
+}
+
+
+logger();
+logger();
+logger();
+
+number(12);
+
+// video 34
+
+function calcAge1(birthYear) {return 2037 - y;}
+const age = calcAge1(y);
+// function expression
+const age2 = calcAge2(1991);
+
+console.log(age1,age2);
+
+// video 35
+
+
+const calcAge3 = function(y) {
+  return 2009 - birthYear;
+}
+
+birthYear => 2009 - birthYear;
+
+const age3 = calcAge3(1991);
+console.log(age3);
+
+const yearsUntilRetirment = birthYear => { const age = 2009 - birthYear
+const retirement = 65 - age;
+return `${firstName} retires in ${ retirement}
+years`;
+
+}
+console.log(yearsUntilRetirment(1991,Jonas));
+console.log(yearsUntilRetirment(2009,Jabril));
+
+//video 36
+
+
+function cutFruitPieces(fruit)
+{
+  return fruit * 4;
+}
+
+function fruitProcessor(apples,oranges)
+{
+  const juice = `juice with ${applePieces} piece of apple and ${orangePieces}
+  pieces of orange.`;
+  return juice;
+}
+console.log(fruitProcessor(2, 3));
+//day37
+
+const calcAverage = (345);
+let scoreDolphins =( 97 + 112 + 80 ) / 3;
+let scoreKoalas = (109 +95 + 50 ) /3;
+console.log(scoreDolphins, scoreKoalas);
+
+if (scoreDolphins > scoreKoalas && scoreDolphins >= 100) {console.log('dolphins win the trophy');}
+else if (scoreKoalas > scoreDolphins && scoreKoalas >= 100) {console.log('koalas win the trophy');}
+else if (scoreKoalas === scoreDolphins && scoreKoalas >= 100) { console.log('koalas win the trophy');}
+else if (scoreDolphins === scoreKoalas && scoreDolphins >= 100 && scoreKoalas >= 100) {console.log('both win tie!')}
+ else {console.log('No one wins the trophy');
+} console.log('no team wins');
+
+checkWinner (scoreDolphins, scoreKoalas);
+
+checkWinner(576, 111);
+
+//test 2
+scoreDolphins = calcAverage(85,54,41);
+scoreKoalas - calcAverage(23,34,27);
+console.log(scoreDolphins,scoreKoalas);
+checkWinner(scoreDolphins, scoreKoalas);
+//video 39
+
+const friend1 = 'jojo'
+const friend2 = 'jabril'
+const friend = ['shit','jojo','fuart'];
+console.log(friends);
+
+const years = new Array(1991, 1984, 2008, 2020);
+
+console.log(friend[0]);
+console.log(friend[2]);
+
+console.log(friends.length);
+console.log(friends[friends. length - 1]);
+friend[2]
+= 'jay';
+ const firstName = 'jonas';
+ const jonas = [firstName, 'jon', 2037-1991,
+'teacher', friends]
+console.log
+
+const calcAge = function (birthyear){return 2037 - birthyear; }
+const years = [1990,1967,2009,2010,2018];
+
+
+const age1 = calcAge(years[0]);
+const age2 = calcAge(years[1]);
+const age3 = calcAge(years[years.length - 1]);
+console.log(age1,age2,age3);
+
+const calcTip = function (bill) { return bill >= 50 && bill <= 300 ? bill * 0.15 : bill *0.2;}
+
+const bills = [125,555,44];
+const tips = [calcTip(bills[0]), calcTip(bills[1]),calcTip(bills)];
+const totals = [bills[0]] + tips[0] + [bills[1]] + [tips[0] , [bills[2] ]+ tips[2]];
+console.log(bills , tips , totals);
+
+//day 20
+
+const jonasArray = [ 'jonas ', 'jabril',
+2009, 1991,
+'teacher','student']
+friend[none];
+
+
+const jabril = ['jabril',
+2009]
+
+const mustafa = {fullName : 'mustafa  abdilahi',
+ mass: 49 };
+
+ const jazmine = {fullName : 'jazmine abdilahi',
+ mass: 89 ,};
+
+
+ console.log('Lifting weights repetition 1');
+ console.log('Lifting weights repetition 2');
+ console.log('Lifting weights repetition 3');
+ console.log('Lifting weights repetition 4');
+ console.log('Lifting weights repetition 5');
+ console.log('Lifting weights repetition 6');
+ console.log('Lifting weights repetition 7');
+ console.log('Lifting weights repetition 8');
+ console.log('Lifting weights repetition 9');
+ console.log('Lifting weights repetition 10');
+
+ for (let rep = 1; rep <= 10; rep++) { console.log(`Lifting weights repetition ${rep}`);}
+
+ const jonasArray = [ 'jonas ',
+ 'jabril',
+2009,
+ 1991,
+'teacher',
+'student']
+friend[
+  none
+];
+
+for(let i =0; i< 5; i++) {
+  console.log(jonas[i])
+}
+console.log(jonas[i], typeof jonas[i]);type.push(typeof jonas[i]);
+
+console.log(types);
+
+const years = [1991,2007,1969,2009];
+const ages = [];
+
+for (let i = 0; i< years.length; i++) {ages.push(2009 - years[i]);
+}
+
+console.log(ages);
+
+console.log('--only strings---')
+for (let i =0; i < jonas.length;
+  i++ )
+{if(typeof jonas[i], typeof jonas[i]);}
+
+
+const jonasArray = [ 'jonas ',
+'jabril',
+2009,
+1991,
+'teacher',
+'student']
+friend[
+ none
+];
+
+for (let i = jonas.length - 1; i >= 0; i--) {console.log(i,jonas[i]);
+}
+
+for (let rep = 1; rep <= 10; rep++)
+{ console.log
+  (`Lifting weights repetition ${rep}`)
+  ;}
+
+
+  for
+  (let exercise =1; exercise <4; exercise++)
+   {
+    console.log
+    (`---------starting exercise${exercise}`)
+}
+
+let dice = Math.trunc(math.random()*6)+1;
+if (dice === 6){
+   console.log('Loop is about to end... ');
+}
+const bill = 100;
+const calcTip = function (bill) {
+  return bill >= 50 && bill <= 300 ? bill * 0.15:
+  bill * 0.2;
+
+  y
+
+  const bills = [22, 295, 176, 440, 37, 105, 10, 1100,];}
+
+for (let i = 1; i < bills.length; i++) {
+  const tip = calcTip(bills[i]);
+  totals.push(tip + bills[i]);
+
+  }
+
+  console. log(bills, tips, totals);
+
+  const calcAverage = function (arr) { console. log(calcAverage([2, 3, 7]));
+    console. log(calcAverage(totals) ) ;
+    console. log(calcAverage(tips) ) ;
+  }
+
+  //feburary 5 1:22 video 51,59(most of it was reading and watching)
+  // video 54
+
+  'use strict';
+
+  const x = '23';
+  if (x=== 23) console.log(23);
+  const calcAge  = birthYear => 2037 - birthYear;
+  console.log();
+// video 70-72
+  const calcTempAmplitude = function (temps) {let max = temps[0]
+    let min = temps[0] }
+    for(let i = 0; i < temps.length; i++) {const curTemp = temps[i]
+    if (typeof curTemp > max) max = curTemp;
+    if (typeof curTemp > min) min = curTemp;
+    console.log(max,min);
+    return max-min; }
+    const AmplitudeNEw = calcTempAmplitude(temperatures);
+    console.log(AmplitudeNEw);
+    const array1 = ['a','b','c'];
+    const array2 = ['d','e','f'];
+    const array3 = array1.concat(array2);
+    //Apr 
+    13 2022
+//notes 91
+// notes 92
+
+//93
+'use strict';
+
+function calcAge(birhtYear) {const age = 2037 - birthYear;
+console.log(firstName);
+return age;}
+const firstName = 'Jonas';
+calcAge(1991);
+//notes 94
+
+//hosting video 95
+var me = 'Jonas';
+let job = 'teacher';
+const year = 1991;
+console.log(me);
+
+function addDecl(a,b) {
+  return a + b;
+
+}
+const addExpr = function(a,b) {
+   return a+b;
+  };
+const addArrow = (a, b)=> a + b;
+ //96 notes
+
+//97 this keyword and arrows
+console.log(this);
+
+const calcAge = function (birthYear)
+{console.log(2037 - birthYear);
+console.log(this);}
+
+calcAge(2009);
+
+const calcAgeArrow = function (birthYear)
+{console.log(2037 - birthYear);
+console.log(this);}
+
+calcAgeArrow(1998);
+
+const Jabril = {
+ year:2009
+  calcAge: function(){
+    console.log(this)
+  }
+};
+// 98
+Jabril.calcAge();
+
+const matilda = {
+  year:2017,
+
+};
+
+matilda.calcAge = Jabril.calcAge;
+matilda.calcAge();
+
+const f = jonas.calcAge;
+f();
+
+const Jabril = {
+  firstName: Jabril
+  calcAge : function(){
+    console.log(this;)
+console.log(2037-this.year);
+  },
+};
+
+const Jabril.farts ();
+jabril.calcAge();
+
+//99 notes
+// 100
+
+let lastName = 'Abdilahi';
+let oldLastName = lastName;
+lastName = 'Mohamed';
+console.log(lastName, oldLastName);
+
+const jessica = {
+  firstName: 'Jabril',
+  lastName: ' Abdilahi',
+  age: 12,
+};
+
+const jabril2 = {firstname: 'Jabril',
+lastName: 'Abdilahi',
+age:12,
+};
+
+Object.assign({}, Jabril2);
+JabrilCopy.lastName = 'Jabril';
+//siblings push
+JabrilCopy.Sibling.push('Jazmine');
+JabrilCopy.Sibling.push('Mustafa');
+JabrilCopy.Sibling.push('Mikail');
+
+console.log('Sibling.push', Jabril);
+console.log('Sibling.push', JabrilCopy);
+//102
+
+const arr = [2,3,4];
+const a = arr[0];
+const b = arr[1];
+const c = arr[2];
+
+const [ x, y, z] = arr;
+console.log(x,y,z);
+
+const [ main, secondary] = restaurant.categories;
+console.log(first, second);
+main = secondary;
+secondary = temp;
+
+const [ starter, mainCourse] = restaurant.order(2,0);
+console.log(starter, mainCourse);
+// /\
+// |
+//103
+
+
+const {name, openingHours, categories } = restaurant;
+console.log(name, openingHours, categories);
+
+const {name: restaurantName, openingHours: hours,
+categories: tags,
+= restaurant;
+console.log(restaurantName, hours,tags);
+const {menu = [], starterMenu: starters = []} = restaurant;
+console.log(menu, starters);
+};
+
+let a = 111;
+let b = 999;
+const obj = a: 23, b: 7, c: 14};
+
+{ a,b = obj;};
+console.log(a,b);
+
+const {fri} = openingHours;
+console.log(fri);
+
+//104
+
+
+const arr = [7,8,9];
+const badNewArr = [1,2, arr[0], arr[1], arr[2]];
+console.log(badNewArr);
+
+const newArr = [1,2, ...arr];
+console.log(newArr);
+
+console.log(...newArr);
+console.log(1,2,7,8,9);
+
+const newMenu = [...restaurant.mainMenu, 'Gnocci'];
+console.log(newMenu);
+
+const mainMenuCopy = [...restaurant.mainMenu];
+
+const menu = [...restaurant.starterMenu, ...
+  restaurant.mainMenu];
+console.log(menu);
+
+const str = 'Jabril';
+const letters = [...str,'','S.'];
+console.log(letters);
+console.log(...str);
+
+//ingredents
+
+const ingredients = [prompt('lets make pasta!
+ingredent 1?'), prompt('Ingredients 2?'),
+prompt('ingredent 3'),];
+
+console.log(ingredients;
+  restaurant.orderPasta(ingredients[0],
+  ingredients[1],
+ingredients[2],);
+
+const;
+
+//106
+
+const arr = [1,2,...[3,4]];
+// REST , because on LEFT side of =
+const [a,b,...others] = [ 1 , 2 , 3 , 4 , 5 ];
+const [pizza,,risotto, ...otherFood]=[...restaurant.mainMenu,
+  ...restaurant.starterMenu,
+];
+
+console.log(pizza,risotto, otherFood);
+
+//Objects
+
+const{sat,...weekdays} = restaurant.openingHours;
+console.log(weekdays);
+//2) Functions
+
+const add = function(...numbers) {
+
+let sum = 0;
+for(let i = 0; i<numbers.length; i+= numbers [i];
+console.log(sum);
+
+};
+
+add(2, 3);
+add(5,3,7,2);
+add(8,2,5,3,2,1,4);
+
+const x =  [ 23, 5, 7 ];
+add(...x);
+
+restaurt.orderPizza('')
+//107
+
+console.log(3 || 'Jabril');
+console.log(''|| 'Jabril');
+console.log(true || 0);
+console.log(underfined || );
+
+console.log(underfined || 0 || '' || 'hello' || 23 ||
+null);
+
+const guests1 = restaurant.numGuests ? restaurant.numGuests : 10;
+console.log(guests1);
+
+const guests2 = restaurant.numGuests || 10
+console.log(guests2);
+
+console.log('---- AND ----');
+console.log(0 && 'Jabril');
+console.log(7 && 'Jabril');
+
+console.log('Hello' && 23 && null && 'jonas');
+
+
+if(restaurant.orderPizza) {
+  restaurant.orderPizza('chicken' , 'extra cheese');
+}
+//108
+
+//109
+
+const rest1 = {
+name: 'Capri',
+numGuests: 20,
+};
+
+const rest1 = {
+name: 'La Piazza',
+owner: 'Jabrilo Abdilahio',
+};
+
+//rest1.numGuests = rest1.numGuest || 10;
+//rest2.numGuests = rest2.numGuest || 10;
+
+rest1.numGuests ??= 10;
+rest1.numGuests ??= 10;
+
+rest2.owner = rest2.owner && '<ANONYMOUS>';
+rest2.owner = rest2.owner && '<ANONYMOUS>';
+
+console.log(rest1);
+console.log(rest2);
+
+
+
+//110 
+
+ const game = { 
+
+  team1: 'Jabril Munich',
+  team2: 'Jazmine Dortmund',
+  PLAYERS: ['Neuer',
+'Pavard',
+'Martinez',
+'Alaba',
+'Davies',
+'Kimmich',
+'Goretzka',
+'Coman',
+'Muller',
+'Gnarby',
+'Lewandowski',
+],
+['Burki',
+'Schulz',
+'Hummels',
+'Akanji',
+'Hakimi',
+'Weigl',
+'Witsel',
+'Hazard',
+'Brandt',
+'snacho',
+score:'4:0'
+scored: [ 'Lewandowski', 'Gnarby', 'Lewandowski',
+'Hummels'],
+date: ' Nov 9th, 2037',
+odds: {team1: 1.33,
+x: 3.25,
+team2: 6.5,
+
+},
+ };
+
+ const [ player1, player2] = game.players;
+ console.log(player1, player2);
+
+ const [ gk, ... fieldPLayers]= players1;
+ console.log(allPLayers);
+
+ const players1Final = [...Players1, 'Thiago', 'Coutinho','Peroscic'];
+
+ const {odds: {team1, x: draw, team2},}= game;
+
+ console.log(team1, draw, team2);
+
+ printGoals = function() {};
+
+ printGoals('Davies', 'Muller', 'Lewandowski','Kimmich');
+ printGoals('Davies', 'Muller', 'Lewandowski',
+ 'Kimmich');
+  
+ printGoals(game.scored);
+
+ team1 < team2 ; console.log('Team1 is more likely to win');
+
+ const menu = [...restaurant.staterMenu, ...restaurant.mainMenu];
+
+ for (const item of menu) console.log(item);
+
+ for(const [i, el] of menu.entries()) { console.log(`${i + 1 }; ${el}`);}
+
+ //112
+
+ const weekdays = [ 'mon', 'tue', 'wed','thu','fri','sat','sun']
+
+ const openingHours = { 
+
+  [weekdays[3]]: {
+
+    open: 12,
+    close: 22,
+
+  },
+
+  [weekdays[4]]: {
+
+    open: 11,
+    close: 23,
+
+
+  },
+
+  [weekdays[5]]: {
+
+    open:0, 
+    close:24,
+  },
+ };
+ // 113 
+if (restaurant.openHours && restaurant.openHours.mon) console.log (restaurant.openingHours.mon.open);
+  
+
+console.log(redtaurant.openingHours.mon.open);
+
+console.log(redtaurant.openingHours.mon?.open);
+
+const days = [ ' mon', 'tue', 'wed', 'thu', 'fri', ' sat', 'sun'];
+
+for(const day of days) { 
+
+  console.log(day);
+  const open = restaurant.openHours[day]?.open || 
+  'closed';
+  console.log(`On ${day}, we open at ${open}`);
+
+  
+}
+//114
+console.log(restaurant.order?.(0, 1) ?? 'Method does not exist');
+
+console.log(restaurant.orderRisotto?.(0, 1) ?? 'Method does not exist');
+
+const users = [{name: ' Jonas', email: 'Hello@jabril.'}];
+
+const user = [];
+
+ console.log(user[0].name ?? 'User array empty');
+
+
+if(user.length > 0) console.log(user[0].name);
+else console.log('user array empty');
+
+const properties = Object.keys(openingHours)
+console.log(properties);
+let openStr = `We are open on${properties}days: `
+;
+for ( const day of Object.keys(openingHours)); {
+  console.log(day);
+}
+
+console.log(openStr);
+
+const values = Object.values(openHours);
+console.log(values);
+
+
+const entries = Object.entries(openingHours);
+for (const x of entires) {
+
+console.log(x);
+
+};
+// 115
+
+
+
+for (const[i,player]of game.scored.entries())
+console.log(`Goeal ${ i + 1}: ${player}`);
+
+let average = 0;
+for (const odd of Object.values(game.odds))
+average += odd;
+
+average += Object.values(game.odds);
+let average = 0;
+console.log(average);
+console.log(average);
